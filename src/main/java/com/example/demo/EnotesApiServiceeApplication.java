50a6fb936aa7126a1d8c7954f1dbc70deec2f2fa
mayursakpal7777 <mayursakpal7777@gmail.com>
2024-10-15 12:54:27 +0530

    initak=l commit

---
+++ src/main/java/com/example/demo/EnotesApiServiceeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesApiServiceeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesApiServiceeApplication.class, args);
+	}
+
+}
